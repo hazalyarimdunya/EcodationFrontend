5e48c585bb858290927ef382e7c1ef0459ee5c43
hazalyarimdunya <hazalyarimduya.49@gmail.com>
2025-08-13 22:05:31 +0300

    jquery - scrol to top

--- JS/template.js
+++ JS/template.js
@@ -30,4 +30,24 @@ $(document).ready(function () {
   });
 });
 
+ /* Back to top icon */
+$(document).ready(function () {
+  // backtop id almak
+  const backTop = $('#back_top_id');
 
+  // scrollTop mesafesi 80 olduğunda gizle
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 200) {
+      backTop.fadeIn(200);
+    } else {
+      backTop.fadeOut(200);
+    }
+  }); 
+  // backtop id'ye tıklandığında sayfayı yukarı kaydır
+  backTop.on("click", function(e){
+    e. preventDefault();
+    $('html').animate({ scrollTop:0},1000,"linear", function(){backTop.fadeOut(200)});
+  })
+
+
+});
